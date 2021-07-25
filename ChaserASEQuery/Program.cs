ad7d4886f720cc22f5fa02045d81caaed728f27e
Cryptorx <63181705+Cryptorx@users.noreply.github.com>
2021-07-25 06:27:48 -0400

    Add project files.

---
+++ ChaserASEQuery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChaserASEQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
